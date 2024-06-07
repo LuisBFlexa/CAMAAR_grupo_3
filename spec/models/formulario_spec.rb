63edf6d882ed6c3e532d76e8da828b1664c12b82
LuisBFlexa <221006389@aluno.unb.br>
2024-06-07 15:34:26 -0300

    Testes das models formulario e formulario_template prontos, feature formulario. USANDO TDD, testes ainda falham

--- spec/models/formulario_spec.rb
+++ spec/models/formulario_spec.rb
@@ -1,1 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Formulario, type: :model do
+  describe 'validations' do
+    context 'with valid attributes' do
+      it 'is valid' do
+        formulario = build(:formulario)
+        expect(formulario).to be_valid
+      end
+    end
+
+    context 'without a nome' do
+      it 'is invalid' do
+        formulario = build(:formulario, nome: nil)
+        expect(formulario).not_to be_valid
+      end
+    end
+
+    context 'without perguntas' do
+        it 'is invalid' do
+          formulario = build(:formulario, perguntas: nil)
+          expect(formulario).not_to be_valid
+        end
+      end
+  end
+
+  describe 'associations' do
+    it { should belong_to(:formulario_template) }
+  end
+end
 
--- spec/models/formulario_template_spec.rb
+++ spec/models/formulario_template_spec.rb
@@ -1,1 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe FormularioTemplate, type: :model do
+  describe 'validations' do
+    context 'with valid attributes' do
+      it 'is valid' do
+        formulario_template = build(:formulario_template)
+        expect(formulario_template).to be_valid
+      end
+    end
+
+    context 'without a nome' do
+      it 'is invalid' do
+        formulario_template = build(:formulario_template, nome: nil)
+        expect(formulario_template).not_to be_valid
+      end
+    end
+  end
+
+  describe 'associations' do
+    it { should have_many(:formularios) }
+  end
+end
 
