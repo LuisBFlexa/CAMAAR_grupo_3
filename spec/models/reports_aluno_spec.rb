4a98af3bc2bd63c6f6f1430b09f791175e3b42e8
LuisBFlexa <221006389@aluno.unb.br>
2024-06-07 15:42:51 -0300

    Testes das models reports prontos, feature formulario. USANDO TDD, testes ainda falham

--- spec/models/reports_aluno_spec.rb
+++ spec/models/reports_aluno_spec.rb
@@ -1,1 +1,15 @@
+require 'rails_helper'
 
+RSpec.describe ReportsAluno, type: :model do
+  describe 'validations' do
+    context 'is valid with valid attributes' do
+      it 'should be valid with valid attributes' do
+        reports_aluno = build(:reports_aluno)
+        expect(reports_aluno).to be_valid
+      end
+    end
+   end
+    describe 'associations' do
+        it { should belong_to(:formularios) }
+    end
+end
