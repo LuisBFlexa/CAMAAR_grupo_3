2e5fc82a83ea8d846fc30d6df7d796a0022e96ec
carlos <carloseduardoarnaud@gmail.com>
2024-06-27 00:42:28 -0300

    testando rubycritic

--- Gemfile
+++ Gemfile
@@ -78,4 +78,5 @@ group :test do
   gem 'faker'
   gem 'shoulda-matchers'
   gem 'rails-controller-testing'
+  gem "rubycritic", require: false
 end
--- app/controllers/reports_professors_controller.rb
+++ app/controllers/reports_professors_controller.rb
@@ -1,5 +1,5 @@
 class ReportsProfessorsController < ApplicationController
-  before_action :set_reports_professor, only: %i[ show edit update destroy ]
+  before_action :set_reports_professor, only: %i[show edit update destroy]
 
   # GET /reports_professors or /reports_professors.json
   def index
@@ -22,49 +22,56 @@ class ReportsProfessorsController < ApplicationController
   # POST /reports_professors or /reports_professors.json
   def create
     @reports_professor = ReportsProfessor.new(reports_professor_params)
-
-    respond_to do |format|
-      if @reports_professor.save
-        format.html { redirect_to reports_professor_url(@reports_professor), notice: "Reports professor was successfully created." }
-        format.json { render :show, status: :created, location: @reports_professor }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @reports_professor.errors, status: :unprocessable_entity }
-      end
+    if @reports_professor.save
+      handle_success(:created, @reports_professor, 'created')
+    else
+      handle_failure(:new)
     end
   end
 
   # PATCH/PUT /reports_professors/1 or /reports_professors/1.json
   def update
-    respond_to do |format|
-      if @reports_professor.update(reports_professor_params)
-        format.html { redirect_to reports_professor_url(@reports_professor), notice: "Reports professor was successfully updated." }
-        format.json { render :show, status: :ok, location: @reports_professor }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @reports_professor.errors, status: :unprocessable_entity }
-      end
+    if @reports_professor.update(reports_professor_params)
+      handle_success(:ok, @reports_professor, 'updated')
+    else
+      handle_failure(:edit)
     end
   end
 
   # DELETE /reports_professors/1 or /reports_professors/1.json
   def destroy
     @reports_professor.destroy!
-
     respond_to do |format|
-      format.html { redirect_to reports_professors_url, notice: "Reports professor was successfully destroyed." }
+      format.html { redirect_to reports_professors_url, notice: 'Reports professor was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reports_professor
-      @reports_professor = ReportsProfessor.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def reports_professor_params
-      params.require(:reports_professor).permit(:formulario_id)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reports_professor
+    @reports_professor = ReportsProfessor.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reports_professor_params
+    params.require(:reports_professor).permit(:formulario_id)
+  end
+
+  # Handle successful save or update
+  def handle_success(status, reports_professor, action)
+    respond_to do |format|
+      format.html { redirect_to reports_professor_url(reports_professor), notice: "Reports professor was successfully #{action}." }
+      format.json { render :show, status: status, location: reports_professor }
     end
+  end
+
+  # Handle failure to save or update
+  def handle_failure(render_action)
+    respond_to do |format|
+      format.html { render render_action, status: :unprocessable_entity }
+      format.json { render json: @reports_professor.errors, status: :unprocessable_entity }
+    end
+  end
 end
