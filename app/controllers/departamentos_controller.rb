c1662f1f7bd8405015cddc89970eae3a05f584d9
carlos <carloseduardoarnaud@gmail.com>
2024-07-04 20:17:59 -0300

    Refatorando

--- app/controllers/departamentos_controller.rb
+++ app/controllers/departamentos_controller.rb
@@ -1,79 +1,70 @@
-# Controller responsável por gerenciar departamentos na aplicação.
 class DepartamentosController < ApplicationController
-  # Antes de executar as ações show, edit, update e destroy, configura o departamento.
-  before_action :set_departamento, only: %i[ show edit update destroy ]
+  before_action :set_departamento, only: %i[show edit update destroy]
 
-  # GET /departamentos ou /departamentos.json
+  # GET /departamentos or /departamentos.json
   def index
-    # Obtém todos os departamentos existentes.
     @departamentos = Departamento.all
   end
 
-  # GET /departamentos/1 ou /departamentos/1.json
+  # GET /departamentos/1 or /departamentos/1.json
   def show
-    # Encontra o departamento pelo ID especificado.
-    @departamento = Departamento.find(params[:id])
   end
 
   # GET /departamentos/new
   def new
-    # Inicializa um novo objeto departamento.
     @departamento = Departamento.new
   end
 
   # GET /departamentos/1/edit
   def edit
-    # Encontra o departamento pelo ID especificado para edição.
-    @departamento = Departamento.find(params[:id])
   end
 
-  # POST /departamentos ou /departamentos.json
+  # POST /departamentos or /departamentos.json
   def create
-    # Cria um novo departamento com os parâmetros permitidos.
     @departamento = Departamento.new(departamento_params)
-
-    respond_to do |format|
-      if @departamento.save
-        format.html { redirect_to departamento_url(@departamento), notice: "Departamento foi criado com sucesso." }
-        format.json { render :show, status: :created, location: @departamento }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @departamento.errors, status: :unprocessable_entity }
-      end
-    end
+    process_request(@departamento.save, :new, "Departamento was successfully created.")
   end
 
-  # PATCH/PUT /departamentos/1 ou /departamentos/1.json
+  # PATCH/PUT /departamentos/1 or /departamentos/1.json
   def update
-    respond_to do |format|
-      if @departamento.update(departamento_params)
-        format.html { redirect_to departamento_url(@departamento), notice: "Departamento foi atualizado com sucesso." }
-        format.json { render :show, status: :ok, location: @departamento }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @departamento.errors, status: :unprocessable_entity }
-      end
-    end
+    process_request(@departamento.update(departamento_params), :edit, "Departamento was successfully updated.")
   end
 
-  # DELETE /departamentos/1 ou /departamentos/1.json
+  # DELETE /departamentos/1 or /departamentos/1.json
   def destroy
     @departamento.destroy
-
-    respond_to do |format|
-      format.html { redirect_to departamentos_url, notice: "Departamento foi excluído com sucesso." }
-      format.json { head :no_content }
-    end
+    process_destroy
   end
 
   private
-    # Usa callbacks para configurar ou impor restrições comuns entre as ações.
-    def set_departamento
-      @departamento = Departamento.find(params[:id])
-    end
 
-    # Permite apenas uma lista de parâmetros confiáveis.
-    def departamento_params
-      params.require(:departamento).permit(:nome)
+  # Use callbacks to share common setup or constraints between actions.
+  def set_departamento
+    @departamento = Departamento.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def departamento_params
+    params.require(:departamento).permit(:nome)
+  end
+
+  # Handle request processing
+  def process_request(success, render_action, success_message)
+    respond_to do |format|
+      if success
+        format.html { redirect_to @departamento, notice: success_message }
+        format.json { render :show, status: :ok, location: @departamento }
+      else
+        format.html { render render_action, status: :unprocessable_entity }
+        format.json { render json: @departamento.errors, status: :unprocessable_entity }
+      end
     end
+  end
+
+  def process_destroy
+    respond_to do |format|
+      format.html { redirect_to departamentos_url, notice: "Departamento was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
 end
